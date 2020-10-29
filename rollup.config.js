2072429de26f83b3df736a587ca06de9981c85d0
Jason Lee <jason@zerodevx.com>
2020-10-29 23:58:26 +0800

    Add legacy build

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,66 @@
 import json from '@rollup/plugin-json'
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
 import livereload from 'rollup-plugin-livereload'
 import serve from 'rollup-plugin-serve'
 
 const production = !process.env.ROLLUP_WATCH
 
-export default {
+const build = {
   input: 'src/index.js',
   output: {
     sourcemap: true,
     format: 'es',
-    file: production ? 'dist/zero-md.min.js' : 'test/build/bundle.js'
+    file: 'dist/zero-md.min.js'
   },
   plugins: [
     json(),
+    resolve(),
+    terser()
+  ]
+}
+
+const buildLegacy = {
+  input: 'src/index.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'ZeroMd',
+    file: 'dist/zero-md.legacy.min.js'
+  },
+  plugins: [
+    json(),
+    resolve(),
+    babel({ babelHelpers: 'bundled' }),
+    commonjs(),
+    terser()
+  ]
+}
+
+const dev = {
+  input: 'src/index.js',
+  output: {
+    sourcemap: true,
+    format: 'es',
+    file: 'test/build/bundle.js'
+  },
+  plugins: [
+    json(),
+    resolve(),
     !production && serve({
       port: 5000,
       contentBase: ['test', '']
     }),
     !production && livereload({
-      watch: ['test', 'docs'],
+      watch: ['test', 'docs', 'dist'],
       extraExts: ['.md']
-    }),
-    production && terser()
+    })
   ],
   watch: {
     clearScreen: false
   }
 }
+
+export default production ? [build, buildLegacy] : dev

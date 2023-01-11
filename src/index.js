adecbcf211f77f5528a44d2f2afbcc6a8bdbe3d1
yashaka <yashaka@gmail.com>
2023-01-11 18:11:06 +0200

    add ButtonsMenu css

--- src/index.js
+++ src/index.js
@@ -83,7 +83,8 @@ export class ZeroMd extends HTMLElement {
       ],
       cssUrls: [
         'https://cdn.jsdelivr.net/gh/sindresorhus/github-markdown-css@4/github-markdown.min.css',
-        'https://cdn.jsdelivr.net/gh/PrismJS/prism@1/themes/prism.min.css'
+        'https://cdn.jsdelivr.net/gh/PrismJS/prism@1/themes/prism.min.css',
+        'https://cdn.jsdelivr.net/gh/automician/buttons-menu-custom-element@98902f0ad3f21fa6a2f3e0d1aa75b6f478557936/build/static/css/main.e5d0f3d9.css'
       ],
       hostCss:
         ':host{display:block;position:relative;contain:content;}:host([hidden]){display:none;}' +
@@ -407,13 +408,6 @@ export class ZeroMd extends HTMLElement {
           }
         })
 
-        const poetryBoldOption = /<!--(.+)poetryBold(.+)-->/i
-        const [, poetryBoldStart, poetryBoldEnd] = md.match(poetryBoldOption) || [null, '__', '__']
-        const boldRegExpRule = [
-          new RegExp(`${poetryBoldStart}(.*?)${poetryBoldEnd}`, 'gmi'),
-          '<b>$1</b>'
-        ]
-
         let tocLinks = []
         const tocStartLevelOption = /<!--TOC>(\d)-->/i
         const [, tocStartLevel] = md.match(tocStartLevelOption) || [null, 0]
@@ -465,6 +459,12 @@ export class ZeroMd extends HTMLElement {
         const mdExtensionsWithId = /\.md#/gim
         md = md.replace(mdExtensionsWithId, `-md${window.location.search}#`)
 
+        const poetryBoldOption = /<!--(.+)poetryBold(.+)-->/i
+        const [, poetryBoldStart, poetryBoldEnd] = md.match(poetryBoldOption) || [null, '__', '__']
+        const boldRegExpRule = [
+          new RegExp(`${poetryBoldStart}(.*?)${poetryBoldEnd}`, 'gmi'),
+          '<b>$1</b>'
+        ]
         // todo: change to ```poetry ... ``` style
         const poetries = /---[a-z]*\n([\s\S]*?)\n---/gim
         // const backTickPoetries = /```poetry[a-z]*\n([\s\S]*?)\n```/gim

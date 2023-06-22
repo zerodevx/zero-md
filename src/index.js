8d49560760ae752f9b0dc5e68cad4f18cf14f8ad
alifeee <alifeeerenn@hotmail.com>
2023-06-22 18:38:24 +0100

    if `getElementById` does not exist, use `document`

--- src/index.js
+++ src/index.js
@@ -150,7 +150,9 @@ export class ZeroMd extends HTMLElement {
   // Scroll to selected element
   goto(id) {
     if (id) {
-      const el = this.root.getElementById(id.substring(1))
+      const el = this.root.getElementById
+        ? this.root.getElementById(id.substring(1))
+        : document.getElementById(id.substring(1))
       if (el) {
         el.scrollIntoView()
       }

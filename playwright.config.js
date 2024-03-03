3a242082a643ef3d34ac573b998d2d95948c8136
Jason Lee <jason@zerodevx.com>
2024-03-03 15:58:50 +0800

    Update `playwright` config

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from '@playwright/test'
 
-const dev = process.env.NODE_ENV === 'development'
-
 export default defineConfig({
   testDir: './src',
   webServer: {
-    command: 'npm run preview',
-    url: `http://localhost:${dev ? '5' : '4'}137`,
-    reuseExistingServer: dev
+    command: 'npm run dev',
+    url: `http://localhost:5173`,
+    reuseExistingServer: true
+  },
+  use: {
+    baseURL: 'http://localhost:5173'
   }
 })

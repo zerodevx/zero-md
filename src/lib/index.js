71f7b7affffad41b83ba48ad83e54671fd724247
Jason Lee <jason@zerodevx.com>
2024-03-14 20:47:09 +0800

    Better code refactoring

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,16 @@
-import ZeroMdBase, { DEFAULT_HOST_CSS, DEFAULT_STYLESHEETS } from './zero-md-base.js'
+import ZeroMdBase, { DEFAULT_HOST_CSS } from './zero-md-base.js'
 import ZeroMd from './zero-md.js'
 
-export { ZeroMdBase, DEFAULT_HOST_CSS, DEFAULT_STYLESHEETS }
+function register() {
+  customElements.define('zero-md', ZeroMd)
+}
+
+if (
+  document.head.querySelector(
+    `script[src="${import.meta.url}"][type="module"],script[data-zero-md-register]`
+  )
+)
+  register()
+
+export { ZeroMdBase, DEFAULT_HOST_CSS, register }
 export default ZeroMd
